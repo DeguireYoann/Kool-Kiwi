05b629d7483eac4e1beb14d4e10730646f1b5b87
ydeguire <yoann.deguire@valtech.com>
2024-03-11 09:35:47 -0400

    use runtimeConfig to hide auth0 clientId and domain

--- plugins/auth0.ts
+++ plugins/auth0.ts
@@ -1,18 +1,19 @@
 import {
     createAuth0,
 } from "@auth0/auth0-vue";
-
 import auth from "~/middleware/auth";
+
+const runtimeConfig = useRuntimeConfig()
 
 export default defineNuxtPlugin((app)=> {
     const auth0 = createAuth0({
-        domain: "dev-khvneu6jloaumhns.us.auth0.com",
-        clientId: "MlIwYA8Y10LCdcz4vMd4T6AzSOssNboI",
+        domain: runtimeConfig.AUTH0_DOMAIN,
+        clientId: runtimeConfig.AUTH0_CLIENTID,
         authorizationParams: {
             redirect_uri: 'http://localhost:3000'
         }
     });
-
+    
     app.vueApp.use(auth0);
 
     addRouteMiddleware('auth', auth, { global: true })
